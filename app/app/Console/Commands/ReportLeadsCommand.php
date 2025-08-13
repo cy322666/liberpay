9af46c47e756972575fac8c90f7788e5ad6daf9a
cy322666 <cy322666@gmail.com>
2025-08-13 17:32:13 +0200

    init

--- app/app/Console/Commands/ReportLeadsCommand.php
+++ app/app/Console/Commands/ReportLeadsCommand.php
@@ -30,7 +30,7 @@ class ReportLeadsCommand extends \Telegram\Bot\Commands\Command
 
     private static function text() : string
     {
-        $text = 'Отчет по лидам (д/н/м)'.PHP_EOL.PHP_EOL;
+        $text = 'Отчет по лидам (д | н | м)'.PHP_EOL.PHP_EOL;
 
         $staffs = Staff::query()
             ->where('group_id', static::$group_leads)
@@ -55,7 +55,7 @@ class ReportLeadsCommand extends \Telegram\Bot\Commands\Command
                 ->where('responsible_id', $staff->staff_id)
                 ->count();
 
-            $text .= $staff->name.' : '.$countDay.'/'.$countWeek.'/'.$countMonth.PHP_EOL;
+            $text .= $staff->name.' : '.$countDay.' | '.$countWeek.' | '.$countMonth.PHP_EOL;
         }
 
         return $text;
