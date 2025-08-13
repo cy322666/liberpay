b11e257d764f60fe218f188d60fd3b5ec4b3d03c
cy322666 <cy322666@gmail.com>
2025-08-13 14:27:54 +0200

    init

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Log::debug('input', Request::all());
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
